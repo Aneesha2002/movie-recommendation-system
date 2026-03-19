9605599e6f5cbbe0b0afb582bebdf562ae45bcb7
Aneesha <aneeshadsouza2002@gmail.com>
2026-03-19 12:54:36 +0530

    Added models.py

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    release_year = models.IntegerField()
+    genres = models.ManyToManyField(Genre, related_name='movies')
+    average_rating = models.FloatField(default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
