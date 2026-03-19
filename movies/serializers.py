6792f0ff0b6b2079d711938529450cf46ae92d02
Aneesha <aneeshadsouza2002@gmail.com>
2026-03-19 12:59:55 +0530

    Added serializers.py

---
+++ movies/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Movie, Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'name']
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True)
+
+    class Meta:
+        model = Movie
+        fields = [
+            'id',
+            'title',
+            'description',
+            'release_year',
+            'genres',
+            'average_rating',
+        ]
