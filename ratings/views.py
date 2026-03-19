07480e358af7493cd3fdf964ad086edf4c8f64ed
Aneesha <aneeshadsouza2002@gmail.com>
2026-03-19 12:57:09 +0530

    Added views.py

--- ratings/views.py
+++ ratings/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Rating
+from .serializers import RatingSerializer
+from django.db.models import Avg
+from movies.models import Movie
 
-# Create your views here.
+
+class RatingCreateView(APIView):
+    def post(self, request):
+        serializer = RatingSerializer(data=request.data)
+
+        if serializer.is_valid():
+            rating = serializer.save()
+
+            # Update average rating
+            movie = rating.movie
+            avg_rating = movie.rating_set.aggregate(Avg('rating'))['rating__avg']
+            movie.average_rating = avg_rating
+            movie.save()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
