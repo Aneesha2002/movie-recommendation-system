76ce120395dfa9892378a004b9c1ae43136b1be5
Aneesha <aneeshadsouza2002@gmail.com>
2026-03-19 12:57:44 +0530

    Added models.py

--- ratings/models.py
+++ ratings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movies.models import Movie
 
-# Create your models here.
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'movie')
+
+    def __str__(self):
+        return f"{self.user} rated {self.movie} ({self.rating})"
